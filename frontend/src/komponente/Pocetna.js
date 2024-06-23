6f89c32f05ebf87094f7d3b50097bf53d364ac52
darko2806 <darko2marko@gmail.com>
2024-06-24 01:54:23 +0200

    Update Pocetna.js

--- frontend/src/komponente/Pocetna.js
+++ frontend/src/komponente/Pocetna.js
@@ -8,9 +8,9 @@ function Pocetna() {
   return (
     <div className='pocetna-container'>
       <h1>VREME JE ZA PUTOVANJA</h1>
-      <p>Dosta je cekanja!</p>
+      <p>Dosta je ČEKANJA!</p>
       <div className="pocetna-btns">
-        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>ZAPOCNI AVANTURU </Button> 
+        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>ZAPOČNI AVANTURU </Button> 
       </div>
     </div>
   )
